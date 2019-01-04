e1d166c0929551dcd48b7772d7597c4ec8c4115d
Sushil Singh <41812352+singh-sushil@users.noreply.github.com>
2019-01-04 11:28:30 +0545

    Add files via upload

---
+++ verifypassword.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string.h>
+#include<fstream>
+using namespace std;
+string writePassword()
+{ 	
+    unsigned long int pwd;
+    string passwd1;
+    cout << "enter your password" << endl;
+    cin >> passwd1;
+    return passwd1;
+}
+string getPassword()
+{ 
+    int i = 0;	
+	char k;
+    char passwd2[50];
+    ifstream infile("text.jpg");
+    while((k = infile.get()) != EOF)
+	{
+		passwd2[i] = k-3;
+		i++;
+	}
+	passwd2[ i ] = '\0';
+    return passwd2;
+}
+bool verifyPassword(string str1,string str2)
+{
+    int a;
+    a = str1.compare(str2);
+    if (a == 0)
+        return true;
+    else return false;
+    
+}
+int main()
+{
+    bool a ;
+    string str1;
+    string str2;
+    str1 = writePassword();
+    str2 = getPassword();
+    a = verifyPassword(str1,str2);
+    if (a == true)
+        cout << "password is verified" << endl;
+    if (a == false)
+        cout << "password is not verified" << endl;
+    return 0;
+}
